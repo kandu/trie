b51ad429c23d97a77e0a57a58a544d18a2367073
ZAN DoYe <357626+kandu@users.noreply.github.com>
2015-08-06 22:35:02 +0800

    init doc

--- trie.mli
+++ trie.mli
@@ -1,20 +1,31 @@
+(** trie tree
+  This module implements the trie tree data structure.
+  All operations are not lazy. Operations like set, unset over a trie have side-effects.
+ *)
+
 module type intf =
   sig
+    (** type of path point *)
     type path
+
+    (** type of trie node *)
     type 'a node
+
+    (** create a new trie tree with or without an element *)
     val create : 'a option -> 'a node
+
+    (** returns the value associated with the path *)
     val get : 'a node -> path list -> 'a option
+
+    (** associate the value with the path *)
     val set : 'a node -> path list -> 'a -> unit
+
+    (** remove an association of the path *)
     val unset : 'a node -> path list -> unit
+
+    (** returns the sub node associated with the path *)
     val sub: 'a node -> path list -> 'a node option
   end
-module Make :
-  functor (H : Core_kernel.Std.Hashtbl.Key) ->
-    sig
-      type 'a node
-      val create : 'a option -> 'a node
-      val get : 'a node -> H.t list -> 'a option
-      val set : 'a node -> H.t list -> 'a -> unit
-      val unset : 'a node -> H.t list -> unit
-      val sub: 'a node -> H.t list -> 'a node option
-    end
+
+module Make (H : Core_kernel.Std.Hashtbl.Key): intf with type path:= H.t
+
